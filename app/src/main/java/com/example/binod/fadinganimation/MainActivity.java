11f2547ed2d56a55b7a96e1a41640847796e0fcc
binod <binodcoder@gmail.com>
2022-03-01 22:35:34 +0545

    fading animaiton

---
+++ app/src/main/java/com/example/binod/fadinganimation/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.binod.fadinganimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+        ImageView imageView=(ImageView) findViewById(R.id.imageView);
+        imageView.animate().alpha(0.5f).setDuration(2000);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
